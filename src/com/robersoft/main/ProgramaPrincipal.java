9e21219667cb428c58fe7adecae06a919c8cea9b
rober <ccprobertosanchezacosta@gmail.com>
2021-10-14 21:16:18 +0100

    Feature:

--- src/com/robersoft/main/ProgramaPrincipal.java
+++ src/com/robersoft/main/ProgramaPrincipal.java
@@ -10,6 +10,8 @@ public class ProgramaPrincipal {
     public static void main(String[] args) {
 
         Scanner intro = new Scanner(System.in);
+
+
         System.out.print("Introduce el numero de espacios para el Zoo");
         int numOfSpaces = intro.nextInt();
         System.out.print("Introduce el numero de animales para el Zoo");
